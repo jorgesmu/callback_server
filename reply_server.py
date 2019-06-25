8a2df27f5ea8c8b9c095df138e2e5413af5bf234
Jorge Smulevici <jsmulevici@gmail.com>
2019-06-25 21:41:45 +0300

    add reply server v1

---
+++ reply_server.py
@@ -0,0 +1,22 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from io import BytesIO
+
+
+class ReplyPostRequestHandler(BaseHTTPRequestHandler):
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        body = self.rfile.read(content_length)
+        self.send_response(200)
+        self.end_headers()
+        response = BytesIO()
+        response.write(b'Headers: ' + str.encode(str(self.headers)))
+        response.write(b'Url: ' + str.encode(self.path))
+        response.write(b'\n\r')
+        response.write(b'Body: ' + str.encode(str(body)))
+        self.wfile.write(response.getvalue())
+
+PORT = 8000
+httpd = HTTPServer(('localhost', 8000), ReplyPostRequestHandler)
+print('Starting server at port: ', PORT)
+httpd.serve_forever()
